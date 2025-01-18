98d50e0d21fb8afaaf06575f9adc582111a8b31a
Sai Chaithanya Vadakattu <111531760+saichaithanya6@users.noreply.github.com>
2025-01-18 17:22:49 -0500

    changes2

---
+++ src/ingest_data.py
@@ -0,0 +1,41 @@
+import os
+import zipfile
+from abc import ABC, abstractmethod
+import pandas as pd
+
+# Abstract class for data ingestion
+class DataIngestion(ABC):
+    @abstractmethod
+    def ingest(self, file_path) -> pd.DataFrame:
+        '''Abstract method to ingest data from a file'''
+        pass
+
+
+#implement a concrete class for ingesting data from a zip file
+class ZipFileDataIngestion(DataIngestion):
+    def ingest(self, file_path: str) -> pd.DataFrame:
+        '''Extracts a zip file and returns the content into a pandas DataFrame'''
+        
+        if not file_path.endswith(".zip"):
+            raise FileNotFoundError(f"File not found: {file_path}")
+        
+        # Extract the zip file
+        with zipfile.ZipFile(file_path, 'r') as zip_ref:
+            zip_ref.extractall("extracted_data")
+        
+        # Find if there is a csv file in the extracted folder
+        extracted_files = os.listdir("extracted_data")
+        csv_files = [f for f in extracted_files if f.endswith(".csv")]
+        
+        if len(csv_files) == 0:
+            raise FileNotFoundError(f"No CSV file found in the zip file: {file_path}")
+        if len(csv_files) > 1:
+            raise ValueError(f"Multiple CSV files found in the zip file: {file_path}")
+        
+        # Read the csv file into a pandas DataFrame
+        csv_file_path = os.path.join("extracted_data", csv_files[0])
+        df = pd.read_csv(csv_file_path)
+        
+        return df
+
+
