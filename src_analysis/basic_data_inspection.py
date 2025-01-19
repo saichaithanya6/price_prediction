928ceb7ae773bd321bdeedffdb061be8f93a4a22
Sai Chaithanya Vadakattu <111531760+saichaithanya6@users.noreply.github.com>
2025-01-19 13:26:54 -0500

    changes4

---
+++ src_analysis/basic_data_inspection.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+import pandas as pd
+
+class DataInspection(ABC):
+    @abstractmethod
+    def inspect(self, data: pd.DataFrame):
+        '''Abstract method to inspect data'''
+        pass
+
+#This strategey inspects the data types of the columns
+
+class DataTypeInspection(DataInspection):
+    def inspect(self, data: pd.DataFrame):
+        '''Inspects the data types of the columns'''
+        return data.info()
+
+class summary_statistics_inspection(DataInspection):
+    def inspect(self, data: pd.DataFrame):
+        '''Inspects the summary statistics of the data'''
+        return data.describe()
+
+
+#This class is the context class that will use the strategy
+class DataInspector:
+    def __init__(self, strategy: DataInspection):
+        '''Initializes the DataInspector with a strategy'''
+        
+        self.strategy = strategy
+    
+    def set_strategy(self, strategy: DataInspection):
+        '''Sets the strategy of the DataInspector'''
+        self.strategy = strategy
+    
+    def execute_strategy(self, data: pd.DataFrame):
+        '''Executes the strategy to inspect the data'''
+        return self.strategy.inspect(data)
+
