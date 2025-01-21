1fc6a15a35492950bbc7ff0c8688775b0057d8a1
Sai Chaithanya Vadakattu <111531760+saichaithanya6@users.noreply.github.com>
2025-01-21 17:34:46 -0500

    changes7

---
+++ src_analysis/univariate_analysis.py
@@ -0,0 +1,56 @@
+from abc import ABC, abstractmethod
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+# Abstract Base Class for Univariate Analysis
+# This class is used to define the structure of the univariate analysis
+
+class UnivariateAnalysisStrategy(ABC):
+    @abstractmethod
+    def analyze(self, data: pd.DataFrame):
+        '''Performs univariate analysis on the data'''
+        pass
+    
+
+# Concrete class for numerical univariate analysis 
+
+class NumericalUnivariateAnalysis(UnivariateAnalysisStrategy):
+    def analyze(self, data:pd.DataFrame, feature: str):
+        '''Performs univariate analysis on numerical data'''
+        
+        plt.figure(figsize=(12, 6))
+        plt.histplot(data[feature], kde=True)
+        plt.title(f'Distribution of {feature}')
+        plt.xlabel(feature)
+        plt.ylabel('Frequency')
+        plt.show()
+
+#Concrete class for categorical univariate analysis
+class CategoricalUnivariateAnalysis(UnivariateAnalysisStrategy):
+    def analyze(self, data:pd.DataFrame, feature: str):
+        '''Performs univariate analysis on categorical data'''
+        
+        plt.figure(figsize=(12, 6))
+        sns.countplot(x= feature, data=data, palette= 'muted')
+        plt.title(f'Distribution of {feature}')
+        plt.xlabel(feature)
+        plt.ylabel('Frequency')
+        plt.show()
+
+#Concrete class that uses univariate analysis
+
+class UnivariateAnalysis:
+    def __init__(self, strategy: UnivariateAnalysisStrategy):
+        """
+        Initializes the UnivariateAnalyzer with a specific analysis strategy.
+        """
+        self.strategy = strategy
+    
+    def set_strategy(self, strategy: UnivariateAnalysisStrategy):
+        '''
+        Sets the strategy for the univariate analysis'''
+        self.strategy = strategy
+        
+    def execute_strategy(self, data: pd.DataFrame, feature: str):
+        self.strategy.analyze(data, feature)
